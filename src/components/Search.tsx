da64e9e612af79472ccdb204ad2661c7bd9c5424
Anmol Singh Tuteja <anmolthedeveloper@gmail.com>
2023-06-09 00:20:08 +0530

    trying to fix selected font color change

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -43,7 +43,6 @@ export default function Search() {
 
   useEffect(() => {
     const element = inputElement.current;
-
     if (!element) return;
 
     element.addEventListener("focusout", (event) => {
